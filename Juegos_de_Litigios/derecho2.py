589f79bba15e39fdb7e41cd571be0b1584b5dacd
lelopez809 <leandrolopezcruz@hotmail.com>
2025-03-12 13:12:30 -0600

    Update derecho2.py

--- Juegos_de_Litigios/derecho2.py
+++ Juegos_de_Litigios/derecho2.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, session, flash
+from flask import Flask, render_template, request, jsonify, redirect, url_for, session, flash
 import sqlite3
 from werkzeug.security import generate_password_hash, check_password_hash
 import smtplib
